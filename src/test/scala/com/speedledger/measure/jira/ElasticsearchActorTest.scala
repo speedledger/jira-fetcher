240962b5b3005df3ac183c89f20aece7d6be8c7f
Tobias Junsten <tobias@junsten.se>
2014-05-22 14:22:44 +0200

    Added a test testing failures

--- src/test/scala/com/speedledger/measure/jira/ElasticsearchActorTest.scala
+++ src/test/scala/com/speedledger/measure/jira/ElasticsearchActorTest.scala
@@ -5,32 +5,42 @@ import org.scalatest.{WordSpecLike, Matchers, BeforeAndAfterAll}
 import akka.actor.{Props, ActorSystem}
 import org.json4s._
 import org.json4s.native.JsonMethods._
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.Future
 import spray.http.{StatusCodes, HttpResponse, HttpRequest}
 
 class ElasticsearchActorTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("JiraActorTest"))
 
-  override def afterAll {
+  override def afterAll() {
     TestKit.shutdownActorSystem(system)
   }
 
   "ElasticSearchActor" must {
+    val json = parse(""" { "test" : "test" } """)
+
+    def success(req: HttpRequest): Future[HttpResponse] = {
+      Future.successful(HttpResponse(status = StatusCodes.OK))
+    }
+
+    def failure(req: HttpRequest): Future[HttpResponse] = {
+      Future.failed(new RuntimeException)
+    }
+
     "Send ack back when processing is done" in {
-      val request = Promise[HttpRequest]()
-      val elastic = TestActorRef(Props(new ElasticSearchActorMock(request)))
-      val json = parse(""" { "test" : "test" } """)
+      val elastic = TestActorRef(Props(new ElasticsearchActorMock(success)))
       elastic ! ElasticData(json, "jira", "issue", "TECH-58", ElasticIssueAck(IssueKey("TECH-58")))
       expectMsg(ElasticIssueAck(IssueKey("TECH-58")))
+    }
+
+    "Send back exception when failure" in {
+      val elastic = TestActorRef(Props(new ElasticsearchActorMock(failure)))
+      elastic ! ElasticData(json, "jira", "issue", "TECH-58", ElasticIssueAck(IssueKey("TECH-58")))
+      expectMsgAllClassOf(classOf[RuntimeException])
     }
   }
 }
 
-class ElasticSearchActorMock(request: Promise[HttpRequest]) extends ElasticsearchActor {
-  // Grab the HTTP request and mock the result
-  override def sendAndReceive = in => {
-    request.success(in)
-    Future.successful(HttpResponse(status = StatusCodes.OK))
-  }
+class ElasticsearchActorMock(f: HttpRequest => Future[HttpResponse]) extends ElasticsearchActor {
+  override def sendAndReceive = f
 }
